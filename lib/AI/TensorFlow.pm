66a771a05ebdbadf20f89dad1f2f2a7a00faf74a
Zakariyya Mughal <zaki.mughal@gmail.com>
2021-06-17 21:39:53 -0400

    Add helper for loading graph into buffer

--- lib/AI/TensorFlow.pm
+++ lib/AI/TensorFlow.pm
@@ -92,18 +92,46 @@ package AI::TensorFlow::DType {
 FFI::C->ffi($ffi);
 
 package AI::TensorFlow::Buffer {#{{{
+	use FFI::Platypus::Buffer;
+	use FFI::Platypus::Memory;
+
 	FFI::C->struct( 'TF_Buffer' => [
 		data => 'opaque',
 		length => 'size_t',
 		_data_deallocator => 'opaque', # data_deallocator_t
 	]);
 
-	$ffi->attach( [ 'NewBuffer' => '_New' ] => [] => 'TF_Buffer' );
-
 	sub data_deallocator {
-		my ($self, $closure) = shift;
+		my ($self, $coderef) = shift;
+
+		return $self->{_data_deallocator_closure} unless $coderef;
+
+		my $closure = $ffi->closure( $coderef );
+
+		$closure->sticky;
+		$self->{_data_deallocator_closure} = $closure;
+
 		my $opaque = $ffi->cast('data_deallocator_t', 'opaque', $closure);
 		$self->_data_deallocator( $opaque );
+	}
+
+	$ffi->attach( [ 'NewBuffer' => '_New' ] => [] => 'TF_Buffer' );
+
+	sub NewFromData { # TODO look at Python high-level API
+		my ($class, $data) = @_;
+
+		my $buf = $class->_New;
+
+		my ($pointer, $size) = scalar_to_buffer $data;
+
+		$buf->data( $pointer );
+		$buf->length( $size );
+		$buf->data_deallocator( sub {
+			my ($pointer, $size) = @_;
+			free $pointer;
+		});
+
+		$buf;
 	}
 
 	$ffi->attach( [ 'DeleteBuffer' => '_Delete' ] => [ 'TF_Buffer' ], 'void' );
--- t/AI/TensorFlow/load_graph.t
+++ t/AI/TensorFlow/load_graph.t
@@ -10,24 +10,13 @@ use Path::Tiny;
 
 use lib 't/lib';
 
-use FFI::Platypus::Buffer;
-use FFI::Platypus::Memory;
-
 subtest "Load graph" => sub {
 	my $model_file = path("t/models/graph.pb");
 	my $ffi = FFI::Platypus->new( api => 1 );
-	my $buf = AI::TensorFlow::Buffer->_New;
 
 	my $data = $model_file->slurp_raw;
-	my ($pointer, $size) = scalar_to_buffer $data;
-	$buf->data( $pointer );
-	$buf->length( $size );
-	my $closure = $ffi->closure(sub {
-		my ($pointer, $size) = @_;
-		free $pointer;
-	});
-	$buf->data_deallocator( $closure );
-	note $buf;
+	my $buf = AI::TensorFlow::Buffer->NewFromData($data);
+	ok $buf;
 
 	my $graph = AI::TensorFlow::Graph->_New;
 	my $status = AI::TensorFlow::Status->_New;
